929a360f5e3386de0c633026514647b248f1e297
max bischof <max@bischof.dk>
2020-08-24 08:16:37 +0200

    Add leaflet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 import { fetchData, getCurrentSamples, csvToArray } from './components/utils'
 import { placesKeys, samplesKeys } from './attributeKeys'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
 export default function App() {
   const proxyURL = 'https://cors-anywhere.herokuapp.com/'
@@ -30,7 +31,7 @@ export default function App() {
           )
           sample.GEOGR_BREITE = place.GEOGR_BREITE
           sample.GEOGR_LAENGE = place.GEOGR_LAENGE
-          return sample
+          return sample 
         })
       )
     )
@@ -38,5 +39,18 @@ export default function App() {
 
   console.log(samples)
 
-  return <div className="App"></div>
+  const position = [51.505, -0.09]
+  const map = (
+  <Map center={position} zoom={13}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+    <Marker position={position}>
+      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+    </Marker>
+  </Map>
+)
+
+  return <div className="App">{map}</div>
 }
